5c302ae8c37e8888669294e0da65b1b9c6314579
unknown <bandanasatpathy435@gmail.com>
2022-02-12 16:24:03 +0530

    this is my 8th commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from "./img/img1.jpeg";
+import img2 from "./img/img2.jpeg";
+import img3 from "./img/img3.jpeg"
+
+const data = {
+    productData:[
+        {
+           id : 1,
+           img : img1,
+           title:'Grapes',
+           desc :"",
+           price :80
+        },
+        {
+            id : 2,
+            img : img2,
+            title:'Watermelon',
+            desc :"",
+            price :50
+         },
+         {
+            id : 3,
+            img : img3,
+            title:'Orange',
+            desc :"",
+            price :120
+         }
+    ],
+};
+
+export default data;
